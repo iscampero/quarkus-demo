9bebcb03ff4f04e415a3115acebbe2064f97290b
iscampero <is.pa.campero@gmail.com>
2025-06-10 22:10:31 -0300

    Update HolaRecurso.java
    commit

--- src/main/java/com/ejemplo/HolaRecurso.java
+++ src/main/java/com/ejemplo/HolaRecurso.java
@@ -1,16 +1,1 @@
-package com.ejemplo;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
-@Path("/hola")
-public class HolaRecurso {
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
-}
